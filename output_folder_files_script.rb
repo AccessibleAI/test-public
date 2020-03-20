d36168cbd047614ca3096aeb8bd21b43853fa2be
singh-amandeep <singh.amandeep@tftus.com>
2020-03-20 16:59:28 +0530

    output_folder_files_script.rb update

--- output_folder_files_script.rb
+++ output_folder_files_script.rb
@@ -1,6 +1,6 @@
 require 'fileutils'
 FileUtils.touch "file_unsynced"
-FileUtils.mkdir "output"
+FileUtils.mkdir "output" unless File.exists?('output')
 Dir.chdir ("output")
 FileUtils.touch "output_file_synced"
 FileUtils.touch "hello.text"
