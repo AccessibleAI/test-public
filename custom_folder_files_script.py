c318ed6ca6012204dd426d9002dc2925ad16dae4
Shubham Sharma <84713622+sharma-shubham-tft@users.noreply.github.com>
2021-07-08 16:09:53 +0530

    Update custom_folder_files_script.py

--- custom_folder_files_script.py
+++ custom_folder_files_script.py
@@ -1,6 +1,6 @@
 import os
 arr =["custom_file_synced","hello.text"] 
-f = open("{}".format("file_unsynced"), "a")
+f = open("file_unsynced", "a")
 f.close()
 if not os.path.exists("custom"):
   os.mkdir("custom")
