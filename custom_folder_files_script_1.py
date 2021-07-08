948a000aef764e4a8aafe26894524f8371c01d66
Shubham Sharma <84713622+sharma-shubham-tft@users.noreply.github.com>
2021-07-08 15:17:03 +0530

    Update custom_folder_files_script_1.py

--- custom_folder_files_script_1.py
+++ custom_folder_files_script_1.py
@@ -2,7 +2,7 @@ import os
 arr =["file_unsynced","custom_file_synced","hello.text"] 
 if not os.path.exists("custom"):
   os.mkdir("custom")
-  for i in arr:
-    f = open("custom/{}.txt".format(i), "a")
-    f.write("Now the file has more content! {}".format(i))
-    f.close()
+for i in arr:
+  f = open("custom/{}.txt".format(i), "a")
+  f.write("Now the file has more content! {}".format(i))
+  f.close()
