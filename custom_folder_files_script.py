db15f353f3c1586c63257bfd467f5be47d6507fb
Shubham Sharma <84713622+sharma-shubham-tft@users.noreply.github.com>
2021-07-08 15:38:36 +0530

    Update custom_folder_files_script.py

--- custom_folder_files_script.py
+++ custom_folder_files_script.py
@@ -1,7 +1,7 @@
 import os
 os.system('touch file_unsynced')
 if not os.path.exists('custom'):
-        os.makedir('custom')
+        os.system('mkdir custom')
 os.system('cd custom')
 os.system('touch custom_file_synced')
 os.system('touch hello.text')
