f139793f232a9c00c0ac1d32b92dc624e7f31b3e
Shubham Sharma <84713622+sharma-shubham-tft@users.noreply.github.com>
2021-07-11 15:07:22 +0530

    Update output_folder_files_script.py

--- output_folder_files_script.py
+++ output_folder_files_script.py
@@ -1,7 +1,10 @@
 import os
-os.system('touch file_unsynced')
-if not os.path.exists('output'):
-        os.makedirs('output')
-os.system('cd output')
-os.system('touch output_file_synced')
-os.system('touch hello.text')
+arr =["output_file_synced","hello.text"]
+f = open("file_unsynced", "a")
+f.close()
+if not os.path.exists("custom"):
+  os.mkdir("custom")
+for i in arr:
+  f = open("custom/{}".format(i), "a")
+  f.write("Now the file has more content! {}".format(i))
+  f.close()
