dc94f1233316d84d04aae0f6c03c88be259a6cf1
Shubham Sharma <84713622+sharma-shubham-tft@users.noreply.github.com>
2021-07-11 15:40:16 +0530

    Update output_folder_files_script.py

--- output_folder_files_script.py
+++ output_folder_files_script.py
@@ -2,9 +2,9 @@ import os
 arr =["output_file_synced","hello.text"]
 f = open("file_unsynced", "a")
 f.close()
-if not os.path.exists("custom"):
-  os.mkdir("custom")
+if not os.path.exists("output"):
+  os.mkdir("output")
 for i in arr:
-  f = open("custom/{}".format(i), "a")
+  f = open("output/{}".format(i), "a")
   f.write("Now the file has more content! {}".format(i))
   f.close()
